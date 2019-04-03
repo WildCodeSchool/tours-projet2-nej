a2a5d39f8d14efb09098e55fb5fe4685c0495430
Eddyantunes <eddyantunes24@gmail.com>
2019-04-03 16:57:30 +0200

    footer avec image sans mention legal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 
 const routes: Routes = [
-  {path: '',
-  component: SearchBarComponent,
-  } 
+  {
+    path: '',
+    component: SearchBarComponent,
+  }
 ];
 
 @NgModule({

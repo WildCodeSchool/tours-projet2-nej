2dcb91bf839deb25080cd1b37e07f6669f3333c6
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-04-04 15:47:33 +0200

    Linting OK

--- src/app/establishment-homepage/establishment-homepage.component.ts
+++ src/app/establishment-homepage/establishment-homepage.component.ts
@@ -3,7 +3,7 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-establishment-homepage',
   templateUrl: './establishment-homepage.component.html',
-  styleUrls: ['./establishment-homepage.component.css']
+  styleUrls: ['./establishment-homepage.component.css'],
 })
 export class EstablishmentHomepageComponent implements OnInit {
   constructor(private modalService: NgbModal) {}
@@ -18,32 +18,38 @@ export class EstablishmentHomepageComponent implements OnInit {
     {
       name: 'Card 1',
       desc: "Description de l'établissement 1",
-      pic: 'https://images.freeimages.com/images/large-previews/00a/english-town-pub-1565179.jpg',
+      pic:
+        'https://images.freeimages.com/images/large-previews/00a/english-town-pub-1565179.jpg',
     },
     {
       name: 'Card 2',
       desc: "Description de l'établissement 2",
-      pic: 'https://images.freeimages.com/images/large-previews/f06/irish-pub-1233434.jpg',
+      pic:
+        'https://images.freeimages.com/images/large-previews/f06/irish-pub-1233434.jpg',
     },
     {
       name: 'Card 3',
       desc: "Description de l'établissement 3",
-      pic: 'https://images.freeimages.com/images/large-previews/b89/irish-pub-1231950.jpg',
+      pic:
+        'https://images.freeimages.com/images/large-previews/b89/irish-pub-1231950.jpg',
     },
     {
       name: 'Card 1',
       desc: "Description de l'établissement 1",
-      pic: 'https://images.freeimages.com/images/large-previews/00a/english-town-pub-1565179.jpg',
+      pic:
+        'https://images.freeimages.com/images/large-previews/00a/english-town-pub-1565179.jpg',
     },
     {
       name: 'Card 2',
       desc: "Description de l'établissement 2",
-      pic: 'https://images.freeimages.com/images/large-previews/f06/irish-pub-1233434.jpg',
+      pic:
+        'https://images.freeimages.com/images/large-previews/f06/irish-pub-1233434.jpg',
     },
     {
       name: 'Card 3',
       desc: "Description de l'établissement 3",
-      pic: 'https://images.freeimages.com/images/large-previews/b89/irish-pub-1231950.jpg',
+      pic:
+        'https://images.freeimages.com/images/large-previews/b89/irish-pub-1231950.jpg',
     },
   ];
 }

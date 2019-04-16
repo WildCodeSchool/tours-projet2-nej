3a6cdbd1e380a7dd4f54892968b15b5187b7274a
Eddyantunes <eddyantunes24@gmail.com>
2019-04-16 14:15:21 +0200

    post-service

--- src/app/common/services/etablishment.service.ts
+++ src/app/common/services/etablishment.service.ts
@@ -3,24 +3,37 @@ import { Etablishment } from '../models/etablishment.models';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class EtablishmentService {
   etablishement: Etablishment;
   private service: HttpClient;
-  // tslint:disable-next-line: variable-name
+
   constructor(paramService: HttpClient) {
     this.service = paramService;
   }
+  // afiche un etablissement
   public getEtablishment(id:string): Observable<Etablishment> {
     const obs1: Observable<any> = this.service.get(
-      `http://open-reza.herokuapp.com:80/api/establishments/${id}`);
+      `http://open-reza.herokuapp.com/api/establishments/${id}`);
 
     return obs1.pipe(
       map((paramEtablishment: any) => {
         return paramEtablishment as Etablishment;
       }),
+    );
+  }
+  // crée un etablissment
+  public postEtablishment(etablishementForm:any): Observable<Etablishment> {
+    const obs2: Observable<any> = this.service.post(
+      'http://open-reza.herokuapp.com/api/establishments', etablishementForm);
+
+    return obs2.pipe(
+        map((paramEtablishment: any) => {
+          return paramEtablishment as Etablishment;
+        }),
     );
   }
 }

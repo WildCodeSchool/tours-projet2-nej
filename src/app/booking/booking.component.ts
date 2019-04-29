5c9ea0191d7d3d1c797a3c550f5d46fef7fddbe8
Eddyantunes <eddyantunes24@gmail.com>
2019-04-29 11:30:29 +0200

    correction #24

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -5,7 +5,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ParamMap, ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { EtablishmentService } from '../common/services/etablishment.service';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
+import { Etablishment } from '../common/models/etablishment.models';
 
 @Component({
   selector: 'app-booking',
@@ -13,14 +14,9 @@ import {Location} from '@angular/common';
   styleUrls: ['./booking.component.css'],
 })
 export class BookingComponent implements OnInit {
-  public bookings: Booking;
-  public etablishments: [] = [];
-  public dateStartUp: any;
-  public dateStartDate: any;
-  public dateStartTime: any;
-  public dateEndUp: any;
-  public dateEndDate: any;
-  public dateEndTime: any;
+  public bookings: Booking[];
+  public etablishments: Etablishment;
+  public id: string;
 
   constructor(
     private service: BookingService,
@@ -28,9 +24,9 @@ export class BookingComponent implements OnInit {
     public route: ActivatedRoute,
     public router: Router,
     private toastr: ToastrService,
-    private serviceEst: EtablishmentService,
+    private serviceBooking: EtablishmentService,
     private location: Location,
-  ) {}
+  ) { }
 
   bookingForm = this.fb.group({
     date: this.fb.group({
@@ -59,62 +55,62 @@ export class BookingComponent implements OnInit {
   ngOnInit() {
     // Route par id
     this.route.paramMap.subscribe((params: ParamMap) => {
-      const id = params.get('id');
-      const est = params.get('est');
+      this.id = params.get('id');
+      const establishmentId = params.get('est');
 
       // Modification administrateur des réservations, récupération de l'ID de réservation
-      if (id) {
-        this.service.getBooking(id).subscribe((bookingValues: Booking) => {
+      if (this.id) {
+        this.service.getBooking(this.id).subscribe((bookingValues: Booking) => {
           // Récupération de getBooking depuis le service
           this.bookings = bookingValues;
           // Le formulaire a pour valeurs par défaut les données récupérées
           this.bookingForm.patchValue(this.bookings);
-          this.dateStartUp = bookingValues.date.start.toString().split('T');
-          this.dateStartDate = this.dateStartUp[0].split('-');
-          this.dateStartTime = this.dateStartUp[1].split(':');
+          const dateStartUp = bookingValues.date.start.toString().split('T');
+          const dateStartDate = dateStartUp[0].split('-');
+          const dateStartTime = dateStartUp[1].split(':');
           this.bookingForm
             .get('date')
             .get('date1')
             .patchValue({
-              year: parseInt(this.dateStartDate[0], 10),
-              month: parseInt(this.dateStartDate[1], 10),
-              day: parseInt(this.dateStartDate[2], 10),
+              year: parseInt(dateStartDate[0], 10),
+              month: parseInt(dateStartDate[1], 10),
+              day: parseInt(dateStartDate[2], 10),
             });
           this.bookingForm
             .get('date')
             .get('start')
             .patchValue({
-              hour: parseInt(this.dateStartTime[0], 10),
-              minute: parseInt(this.dateStartTime[1], 10),
+              hour: parseInt(dateStartTime[0], 10),
+              minute: parseInt(dateStartTime[1], 10),
             });
 
-          this.dateEndUp = bookingValues.date.end.toString().split('T');
-          this.dateEndDate = this.dateEndUp[0].split('-');
-          this.dateEndTime = this.dateEndUp[1].split(':');
+          const dateEndUp = bookingValues.date.end.toString().split('T');
+          const dateEndDate = dateEndUp[0].split('-');
+          const dateEndTime = dateEndUp[1].split(':');
           this.bookingForm
             .get('date')
             .get('date2')
             .patchValue({
-              year: parseInt(this.dateEndDate[0], 10),
-              month: parseInt(this.dateEndDate[1], 10),
-              day: parseInt(this.dateEndDate[2], 10),
+              year: parseInt(dateEndDate[0], 10),
+              month: parseInt(dateEndDate[1], 10),
+              day: parseInt(dateEndDate[2], 10),
             });
           this.bookingForm
             .get('date')
             .get('end')
             .patchValue({
-              hour: parseInt(this.dateEndTime[0], 10),
-              minute: parseInt(this.dateEndTime[1], 10),
+              hour: parseInt(dateEndTime[0], 10),
+              minute: parseInt(dateEndTime[1], 10),
             });
         });
       }
       // Réservation utilisateurs, récupération de l'ID de l'établissement
-      if (est) {
-        this.serviceEst
-          .getEtablishment(est)
+      if (establishmentId) {
+        this.serviceBooking
+          .getEtablishment(establishmentId)
           .subscribe((etablishmentValue: any) => {
             this.etablishments = etablishmentValue;
-            this.bookingForm.controls.establishment.patchValue(est);
+            this.bookingForm.controls.establishment.patchValue(establishmentId);
           });
       }
     });
@@ -124,26 +120,21 @@ export class BookingComponent implements OnInit {
   public deleteBooking() {
     const result = confirm('Confirmez-vous la suppression de la réservation ?');
     if (result) {
-      this.route.paramMap.subscribe((params: ParamMap) => {
-        const id = params.get('id');
-        this.service.deleteBooking(id).subscribe(() => {
-          this.router.navigate(['']);
-        });
+      this.service.deleteBooking(this.id).subscribe(() => {
+        this.router.navigate(['']);
       });
       this.toastr.warning(
-      'La réservation a bien été supprimée',
-      'Suppression', {
-        positionClass: 'toast-bottom-full-width',
-      },
-    );
+        'La réservation a bien été supprimée',
+        'Suppression', {
+          positionClass: 'toast-bottom-full-width',
+        },
+      );
     }
   }
 
   onSubmit() {
     // appel le champs date du formulaire
     const formDate = this.bookingForm.get('date');
-    console.log(formDate.get('date1').value);
-    console.log(formDate.get('start').value);
     // definis les champs de la date de début
     const dateStart = new Date(
       formDate.get('date1').value.year,
@@ -153,7 +144,6 @@ export class BookingComponent implements OnInit {
       formDate.get('start').value.minute,
       formDate.get('start').value.second,
     );
-    console.log(dateStart);
     // definis les champs de la date de fin
     const dateEnd = new Date(
       formDate.get('date2').value.year,
@@ -164,49 +154,43 @@ export class BookingComponent implements OnInit {
       formDate.get('end').value.second,
     );
 
-    console.log(dateEnd);
     // rassemble les champs de dateSart/dateEnd pour avoir le bon format(date)
     const booking = this.bookingForm.value;
     booking.date = {
       start: dateStart,
       end: dateEnd,
     };
-    // Vérification des données saisies
-    this.route.paramMap.subscribe((params: ParamMap) => {
-      const id = params.get('id');
-      console.log(id);
-      // Si id, MAJ des données
-      if (id) {
-        this.service
-          .updateBooking(this.bookingForm.value, id)
-          .subscribe((newbookingValues: Booking) => {
-            this.bookings = newbookingValues;
-            this.toastr.success(
-              'La réservation a bien été modifié',
-              'Modification',
-              {
-                positionClass: 'toast-bottom-full-width',
-              },
-            );
-          });
-        // Sans id, création d'une nouvelle réservation
-      } else {
-        this.service
-          .createBooking(this.bookingForm.value)
-          .subscribe((newbookingValues: Booking) => {
-            this.bookings = newbookingValues;
-            this.toastr.success(
-              'La réservation a bien été enregistré',
-              'Modification',
-              {
-                positionClass: 'toast-bottom-full-width',
-              },
-            );
-          });
-      }
-    });
+    // Si id, MAJ des données
+    if (this.id) {
+      this.service
+        .updateBooking(this.bookingForm.value, this.id)
+        .subscribe((newbookingValues: Booking) => {
+          this.bookings = newbookingValues;
+          this.toastr.success(
+            'La réservation a bien été modifié',
+            'Modification',
+            {
+              positionClass: 'toast-bottom-full-width',
+            },
+          );
+        });
+      // Sans id, création d'une nouvelle réservation
+    } else {
+      this.service
+        .createBooking(this.bookingForm.value)
+        .subscribe((newbookingValues: Booking) => {
+          this.bookings = newbookingValues;
+          this.toastr.success(
+            'La réservation a bien été enregistré',
+            'Modification',
+            {
+              positionClass: 'toast-bottom-full-width',
+            },
+          );
+        });
+    }
   }
-// Revenir à la page précédente
+  // Revenir à la page précédente
   public previousPage() {
     this.location.back();
   }
